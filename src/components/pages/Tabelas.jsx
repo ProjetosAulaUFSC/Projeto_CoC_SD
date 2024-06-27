06b4fe428bee5c9063f4fc69138021532d579184
Gustavo-Guaresi <gustavoo.guaresi@gmail.com>
2024-06-27 11:16:29 -0300

    tecnicamente ta funcionando metade da tabela

--- src/components/pages/Tabelas.jsx
+++ src/components/pages/Tabelas.jsx
@@ -2,36 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Tabela3x4 from '../form/Table3x4'
 
 function Tabelas(){
-
-    const [dbData, setDbData] = useState(null);
-
-    useEffect(() => {
-        // Função para buscar dados do backend
-        // const fetchDbData = async () => {
-        //   try {
-        //     const response = await fetch('http://localhost:3000/currentDB') // ajuste o URL conforme necessário
-        //     const data = await response.json();
-        //     setDbData(data); // Definir o estado com os dados recebidos
-        //   } catch (error) {
-        //     console.error('Erro ao buscar dados do backend:', error);
-        //   }
-        // };
-    
-        // fetchDbData();
-
-
-              
-        fetch('http://localhost:3000/currentDB', {
-            method: 'GET',
-            headers:{'Content-type':'application/json'},
-        }).then((response)=>response.json()).then((response)=>setDbData(response)).catch((err)=>console.log(err))    
-        
-      }, []);
-    
-      if (!dbData) {
-        return <div>Carregando...</div>; // Mostrar um indicador de carregamento enquanto os dados não chegam
-      }
-
     // Dados para a primeira tabela
     const tableData1 = {
         header: [' ', 'Banco 1', 'Banco 2', 'Banco 3'],
@@ -58,6 +28,25 @@ function Tabelas(){
         ]
     };
 
+    const [dbData, setDbData] = useState(null);
+
+    const [flag, setflag] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/currentDB', {
+            method: 'GET',
+            headers:{'Content-type':'application/json'},
+        }).then((response)=>response.json()).then((response)=>setDbData(response)).catch((err)=>console.log(err))
+        // .then(console.log(dbData))
+      }, []);  
+    
+
+
+      if (!dbData) {
+        return <div>Carregando...</div>; // Mostrar um indicador de carregamento enquanto os dados não chegam
+      }
+
+      tableData1.rows[0][dbData.id] = "X"
 
     return (
         <div>
