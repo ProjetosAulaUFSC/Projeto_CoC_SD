129bbb630faaffd257cfc25025aac885570c8d72
rodrigomarchiolli <rodrigo_nsmarchiolli@hotmail.com>
2024-06-27 08:33:31 -0300

    Sequencidor Fixo segurando os processos

--- backend/utils/websocket.js
+++ backend/utils/websocket.js
@@ -1,8 +1,9 @@
 const WebSocket = require('ws');
-const { put_post_character, find_in_db } = require('../utils/utils');
+const { put_post_character, find_in_db, connect_db } = require('../utils/utils');
 
 let clients = [];
 let requestQueue = [];
+let processing = false;
 
 function initializeWebSocket(server) {
     const wss = new WebSocket.Server({ server });
@@ -11,13 +12,15 @@ function initializeWebSocket(server) {
         clients.push(ws);
         console.log('Novo cliente conectado. Total de clientes:', clients.length);
 
-        ws.on('message', (message) => {
+        ws.on('message', async (message) => {
             console.log('Mensagem recebida:', message);
 
             const wsRequest = { req: JSON.parse(message), ws: ws };
             requestQueue.push(wsRequest);
             requestQueue.sort((a, b) => a.req.timestamp - b.req.timestamp);  // Ordena por timestamp
-            processQueue();
+            if (!processing) {
+                await processQueue();
+            }
         });
 
         ws.on('close', () => {
@@ -30,11 +33,15 @@ function initializeWebSocket(server) {
 }
 
 async function processQueue() {
+    if (processing) return;
+    processing = true;
+
     while (requestQueue.length > 0) {
         const currentRequest = requestQueue.shift();
         const { req, ws } = currentRequest;
 
         try {
+            await connect_db();  // Garante que a conexão com o banco de dados está estabelecida
             let response;
             if (req.type === 'find') {
                 response = await handleFindRequest(req);
@@ -49,6 +56,8 @@ async function processQueue() {
             ws.send(JSON.stringify({ status: 500, message: error.message }));
         }
     }
+
+    processing = false;
 }
 
 async function handleFindRequest(req) {
